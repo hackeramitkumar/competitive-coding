c8a19ae7d622a3ec526e765df6f4babd6cb3219c
hackeramitkumar <amit9116260192@gmail.com>
2022-12-03 10:39:46 +0530

    gg

--- q1.c
+++ q1.c
@@ -5,4 +5,5 @@ int main(){
     cout<<a<<endl;
     //I do not kn0ow what is happening.
     what are you doing I do not know bro.
+    hye bro what is your name , I just want to know your name and address.
 }
