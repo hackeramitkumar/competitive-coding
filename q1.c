2f1189cde970b4afcbc6c5d3347d38f6d949ba17
hackeramitkumar <amit9116260192@gmail.com>
2022-12-03 10:40:16 +0530

    gg

--- q1.c
+++ q1.c
@@ -6,4 +6,5 @@ int main(){
     //I do not kn0ow what is happening.
     what are you doing I do not know bro.
     hye bro what is your name , I just want to know your name and address.
+    O beta ji , kismat ki hawa kabhi naram kabhi garam . namram garam hai garam
 }
